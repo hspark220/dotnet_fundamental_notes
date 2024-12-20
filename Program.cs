67aeab3aabe54355e405d291454f4b49622a52f7
jasper-parc <jasper.park2023@gmail.com>
2024-12-20 11:00:12 -0500

    Clear up files and have the most basic app to run

--- Program.cs
+++ Program.cs
@@ -1,73 +1,15 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-
-builder.Services.AddSingleton<SomeService>();
-builder.Services.AddTransient<IMyLogic, MyLogic>();
-
-IHost app = builder.Build();
-
-// Can run by running app or just run a service??
-await app.Services.GetRequiredService<SomeService>().StartAsync();
-
-//await app.RunAsync();
-
-// Background Service
-public class PrintService : BackgroundService
+public class Program
 {
-    private readonly ILogger _logger;
-    public PrintService(ILogger<PrintService> logger)
+    public static async Task Main(String[] args)
     {
-        _logger = logger;
-    }
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Hello from the printing service!");
-            await Task.Delay(1000, stoppingToken);
-        }
-    }
-}
+        IHost app = builder.Build();
 
-// Running sample app
-class SomeService
-{
-    private IHostEnvironment _env;
-    private ILogger<SomeService> _logger;
-    private IConfiguration _config;
-    private IMyLogic _logic;
-    public SomeService(IHostEnvironment env, ILogger<SomeService> logger, IConfiguration config, IMyLogic logic)
-    {
-        _env = env;
-        _logger = logger;
-        _config = config;
-        _logic = logic;
-        
-    }
-    public Task StartAsync()
-    {
-        _logger.LogInformation($"{_env.ContentRootFileProvider}");
-        _logger.LogInformation($"Value1 : {_config["App:Value1"]} ,,, Value2 : {_config["App:Value2"]}");
-        _logic.Say("Hello World!");
-        return Task.CompletedTask;
-    }
-}
-
-// Sample DI and Logger
-public interface IMyLogic
-{
-    void Say(string message);
-}
-
-public class MyLogic : IMyLogic
-{
-    public void Say(string message)
-    {
-        Console.WriteLine(message);
+        await app.RunAsync();
     }
 }
--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Some notes from reading .NET fundamentals documentation at https://learn.microso
 
 ## Table of Contents
 - [Day 1](#day-1)
-- [Day 2](#day-2)
 
 
 
@@ -12,7 +11,7 @@ Some notes from reading .NET fundamentals documentation at https://learn.microso
 - Hostbuilder [HostBuilder (generic host)](#hostbuilder-generic-host) the base of the app for hosting and running the app
 - Have something running by the end of the day
 
-### HostBuilder (generic host)
+## HostBuilder (generic host)
 Using Microsoft.Extension.Hosting NuGet package to create a generic host that is responsible for app startup and lifetime management. A host is an object that encapsulates an app's resource and lifetime functionality, such as:
 
 - Dependency Injection (DI)
@@ -29,14 +28,3 @@ IHost host = builder.Build()
 - Calls a CreateApplicationBUilder method to create and configure a builder object
 - Calls `Build()` to create an `IHost` instance
 - Calls `Run()` or `RunAsync()` method on the host object--`Run()` calls the `RunAsync()`
-
-## Day 2
-- Fortmat overview?
-- Running a console app through generic host
-
-### Format numbers, dates, other types
-Main purpose is to change different data types into string using the `Object.ToString()` method--which can also be overridden to return different results. Another reason would be to conver the datatype into different types of string for different data standards, eg. representing a number in binary or hexadecimal.
-
-### Console App via generic host
-After going through multiple resources and reading through a couple of blogs, I found this [article](https://codingfreaks.de/consoleapp-01/) to be the most straight forward,,, there still some questions left unanswered but at least I can run a basic console app, use Dependency Injection and do some basic logging.
-One question I still how I can run the app using `await app.RunAsync();` instead of `await app.Services.GetRequiredService<SomeService>().StartAsync();`
